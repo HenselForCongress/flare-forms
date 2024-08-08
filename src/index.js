0fa3ccbb6519b47a3464d11e43eb7cacfd57b8ff
Bentley Hensel <bentleyhensel@gmail.com>
2024-08-08 15:27:04 -0400

    Additional data collection & add to ESP
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const allowedOrigins = [
       'https://henselforcongress.com',
-      /\.henselforcongress\.com$/
+      /\\.henselforcongress\\.com$/
     ];
 
     const origin = request.headers.get('Origin');
@@ -40,13 +40,47 @@ export default {
       const { first = '', last = '', email, zip = '' } = data;
       const source_ip = request.headers.get('CF-Connecting-IP');
       const source_url = request.headers.get('Referer');
+      const country = request.cf ? request.cf.country || 'N/A' : 'N/A';
+      const region = request.cf ? request.cf.region || 'N/A' : 'N/A';
+      const city = request.cf ? request.cf.city || 'N/A' : 'N/A';
+      const timezone = request.cf ? request.cf.timezone || 'N/A' : 'N/A';
 
       const db = env.cloudflareD1;
 
-      const result = await db.prepare(`
-        INSERT INTO signups (first_name, last_name, email_address, zip_code, source_ip, source_url)
-        VALUES (?, ?, ?, ?, ?, ?)
-      `).bind(first, last, email, zip, source_ip, source_url).run();
+      // Insert signup into D1
+      const dbResult = await db.prepare(`
+        INSERT INTO signups (first_name, last_name, email_address, zip_code, source_ip, source_url, country, region, city, timezone)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `).bind(first, last, email, zip, source_ip, source_url, country, region, city, timezone).run();
+
+      const listmonkData = {
+          email: email,
+          name: `${first} ${last}`,
+          status: 'enabled',
+          lists: [6], // List ID for the subscription
+          attribs: {
+            // Add future fields here
+            source_ip: source_ip,
+            source_url: source_url,
+            zip: zip,
+            city: city,
+            region: region,
+            country: country,
+            timezone: timezone
+          }
+        };
+
+      const listmonkResponse = fetch(`${env.ESP_URL}/api/subscribers`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic ' + btoa(`${env.CLOUDFLARE_ACCESS_CLIENT_ID}:${env.CLOUDFLARE_ACCESS_CLIENT_SECRET}`)
+        },
+        body: JSON.stringify(listmonkData)
+      });
+
+      // Wait for both requests to complete
+      await Promise.allSettled([dbResult, listmonkResponse]);
 
       return new Response('Signup successful', {
         headers: { 'Access-Control-Allow-Origin': origin },
